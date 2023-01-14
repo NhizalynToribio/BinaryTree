4eaec8f2b4417ea480d405fcb4d5d91082ca9a6e
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-14 17:25:03 +0800

    Class Section of Binary Tree

--- Binary Tree Exercise.py
+++ Binary Tree Exercise.py
@@ -2,3 +2,12 @@ print("**************** PROGRAMMED BY: *****************")
 print("************** NHIZALYN TORIBIO ****************")
 print("*************** BSCOE 2 - 2 *******************")
 print("*************** BINARY TREE ********************")
+
+
+# This is the Class Section of the Binary Tree Search Node
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
