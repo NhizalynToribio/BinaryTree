a04fd35030471aad6f3f2ef57f39309ec23183a1
NhizalynToribio <nhizalyntoribio03262003@gmail.com>
2023-01-14 22:57:42 +0800

    part 2, start of deleting a letter in binary tree

---
+++ Part2 Binary Tree Exercise.py
@@ -0,0 +1,13 @@
+print("**************** PROGRAMMED BY: *****************")
+print("************** NHIZALYN TORIBIO ****************")
+print("*************** BSCOE 2 - 2 *******************")
+print("*************** BINARY TREE ********************")
+print("******** PART - 2 - DELETING A LETTER *********")
+
+
+# Thi is the class statement of the Deleting a Letter in a Binary Tree
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
